abbceb592d265f2eb908dc2cf3c461414530b15f
Sandhyaili <42805290+Sandhyaili@users.noreply.github.com>
2018-09-19 18:34:00 +0530

    Create function.c

---
+++ function.c
@@ -0,0 +1,13 @@
+A function declaration tells the compiler about a function's name, return type, and parameters. A function definition provides the 
+actual body of the function.
+what is meant by function declaration?
+
+Function declaration is required when you define a function in one source file and you call that function in another file. In such case, you should declare the function at the top of the file 
+calling the function.
+i didnt understand the above statement?
+
+When a program calls a function, the program control is transferred to the called function.
+whatb is progarm control?
+
+
+
