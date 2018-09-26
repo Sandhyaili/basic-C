5f42fd887f106689e4f1ba62896d4bc5d1784905
Sandhyaili <42805290+Sandhyaili@users.noreply.github.com>
2018-09-26 17:26:45 +0530

    Update function.c

--- function.c
+++ function.c
@@ -51,5 +51,27 @@ int max(int num1,int num2)
 }
 output max is 200
 
+  
+  
+#include <stdio.h>
+int max(int num1,int num2)
+{
+    int result;
+    if(num1>num2)
+    result=num1;
+    else
+    result=num2;
+    return result;
+}
+int main()
+{
+    int a = 100;
+    int b = 200;
+    int ret;
+    ret=max( a , b );
+    printf("max is :%d\n" , ret);
+    return 0;
+}
+output max is 200
 
 
