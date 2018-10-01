702e1745c83ae9c0d4a4bbeb1f7810c4a8b8e8d8
Sandhyaili <42805290+Sandhyaili@users.noreply.github.com>
2018-10-01 18:24:20 +0530

    Update array.c

--- array.c
+++ array.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+int main()
+{
+    int n[10];
+    int i;
+    for(i=0;i<10;i++)
+    {
+        n[i]=i+100;
+        printf("element[%d]=%d\n", i,n[i]);
+        
+    }
+}
+output:
+element[0]=100                                                                                                                   
+element[1]=101                                                                                                                   
+element[2]=102                                                                                                                   
+element[3]=103                                                                                                                   
+element[4]=104                                                                                                                   
+element[5]=105                                                                                                                   
+element[6]=106                                                                                                                   
+element[7]=107                                                                                                                   
+element[8]=108                                                                                                                   
+element[9]=109 
